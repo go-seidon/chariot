312e50758c2365c8e5ed19026130a0a51c9ab652
KokoRaka <raka.suryaardi@gmail.com>
2022-11-13 13:42:44 +0700

    update db migration

---
+++ migration/mysql/20221113055448_barrel-index.down.sql
@@ -0,0 +1,4 @@
+
+ALTER TABLE `barrel` DROP INDEX `idx_provider`;
+
+ALTER TABLE `barrel` DROP INDEX `idx_status`;
---
+++ migration/mysql/20221113055448_barrel-index.up.sql
@@ -0,0 +1,4 @@
+
+ALTER TABLE `barrel` ADD INDEX idx_provider(`provider`);
+
+ALTER TABLE `barrel` ADD INDEX idx_status(`status`);
