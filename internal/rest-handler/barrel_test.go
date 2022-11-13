b1888ea360856f7cc19b2a862f9185c0a244f33f
KokoRaka <raka.suryaardi@gmail.com>
2022-11-13 23:00:29 +0700

    add update barrel by id handler implementation

--- internal/rest-handler/barrel_test.go
+++ internal/rest-handler/barrel_test.go
@@ -326,4 +326,190 @@ var _ = Describe("Auth Handler", func() {
 			})
 		})
 	})
+
+	Context("UpdateBarrelById function", Label("unit"), func() {
+		var (
+			currentTs    time.Time
+			ctx          echo.Context
+			h            func(ctx echo.Context) error
+			rec          *httptest.ResponseRecorder
+			barrelClient *mock_barrel.MockBarrel
+			updateParam  barrel.UpdateBarrelByIdParam
+			updateRes    *barrel.UpdateBarrelByIdResult
+		)
+
+		BeforeEach(func() {
+			currentTs = time.Now().UTC()
+			reqBody := &rest_app.UpdateBarrelByIdRequest{
+				Code:     "code",
+				Name:     "name",
+				Provider: "goseidon_hippo",
+				Status:   "active",
+			}
+			body, _ := json.Marshal(reqBody)
+			buffer := bytes.NewBuffer(body)
+			req := httptest.NewRequest(http.MethodPost, "/", buffer)
+			req.Header.Set(echo.HeaderContentType, echo.MIMEApplicationJSON)
+			rec = httptest.NewRecorder()
+
+			e := echo.New()
+			ctx = e.NewContext(req, rec)
+			ctx.SetParamNames("id")
+			ctx.SetParamValues("mock-id")
+
+			t := GinkgoT()
+			ctrl := gomock.NewController(t)
+			barrelClient = mock_barrel.NewMockBarrel(ctrl)
+			barrelHandler := rest_handler.NewBarrel(rest_handler.BarrelParam{
+				Barrel: barrelClient,
+			})
+			h = barrelHandler.UpdateBarrelById
+			updateParam = barrel.UpdateBarrelByIdParam{
+				Id:       "mock-id",
+				Code:     reqBody.Code,
+				Name:     reqBody.Name,
+				Provider: string(reqBody.Provider),
+				Status:   string(reqBody.Status),
+			}
+			updateRes = &barrel.UpdateBarrelByIdResult{
+				Success: system.SystemSuccess{
+					Code:    1000,
+					Message: "success update barrel",
+				},
+				Id:        "id",
+				Code:      "code",
+				Name:      "name",
+				Provider:  "goseidon_hippo",
+				Status:    "active",
+				CreatedAt: currentTs,
+				UpdatedAt: currentTs,
+			}
+		})
+
+		When("failed binding request body", func() {
+			It("should return error", func() {
+				body, _ := json.Marshal(struct {
+					Name int `json:"name"`
+				}{
+					Name: 1,
+				})
+				buffer := bytes.NewBuffer(body)
+
+				req := httptest.NewRequest(http.MethodPost, "/", buffer)
+				req.Header.Set(echo.HeaderContentType, echo.MIMEApplicationJSON)
+				rec := httptest.NewRecorder()
+
+				e := echo.New()
+				ctx := e.NewContext(req, rec)
+
+				err := h(ctx)
+
+				Expect(err).To(Equal(&echo.HTTPError{
+					Code: 400,
+					Message: &rest_app.ResponseBodyInfo{
+						Code:    1002,
+						Message: "invalid request",
+					},
+				}))
+			})
+		})
+
+		When("there is invalid data", func() {
+			It("should return error", func() {
+				barrelClient.
+					EXPECT().
+					UpdateBarrelById(gomock.Eq(ctx.Request().Context()), gomock.Eq(updateParam)).
+					Return(nil, &system.SystemError{
+						Code:    1002,
+						Message: "invalid data",
+					}).
+					Times(1)
+
+				err := h(ctx)
+
+				Expect(err).To(Equal(&echo.HTTPError{
+					Code: 400,
+					Message: &rest_app.ResponseBodyInfo{
+						Code:    1002,
+						Message: "invalid data",
+					},
+				}))
+			})
+		})
+
+		When("barrel is not available", func() {
+			It("should return error", func() {
+				barrelClient.
+					EXPECT().
+					UpdateBarrelById(gomock.Eq(ctx.Request().Context()), gomock.Eq(updateParam)).
+					Return(nil, &system.SystemError{
+						Code:    1004,
+						Message: "barrel is not available",
+					}).
+					Times(1)
+
+				err := h(ctx)
+
+				Expect(err).To(Equal(&echo.HTTPError{
+					Code: 404,
+					Message: &rest_app.ResponseBodyInfo{
+						Code:    1004,
+						Message: "barrel is not available",
+					},
+				}))
+			})
+		})
+
+		When("failed update barrel", func() {
+			It("should return error", func() {
+				barrelClient.
+					EXPECT().
+					UpdateBarrelById(gomock.Eq(ctx.Request().Context()), gomock.Eq(updateParam)).
+					Return(nil, &system.SystemError{
+						Code:    1001,
+						Message: "network error",
+					}).
+					Times(1)
+
+				err := h(ctx)
+
+				Expect(err).To(Equal(&echo.HTTPError{
+					Code: 500,
+					Message: &rest_app.ResponseBodyInfo{
+						Code:    1001,
+						Message: "network error",
+					},
+				}))
+			})
+		})
+
+		When("success update barrel", func() {
+			It("should return result", func() {
+				barrelClient.
+					EXPECT().
+					UpdateBarrelById(gomock.Eq(ctx.Request().Context()), gomock.Eq(updateParam)).
+					Return(updateRes, nil).
+					Times(1)
+
+				err := h(ctx)
+
+				res := &rest_app.UpdateBarrelByIdResponse{}
+				json.Unmarshal(rec.Body.Bytes(), res)
+
+				Expect(err).To(BeNil())
+				Expect(rec.Code).To(Equal(http.StatusOK))
+				Expect(res.Code).To(Equal(int32(1000)))
+				Expect(res.Message).To(Equal("success update barrel"))
+				Expect(res.Data).To(Equal(rest_app.UpdateBarrelByIdData{
+					Id:        updateRes.Id,
+					Code:      updateRes.Code,
+					Name:      updateRes.Name,
+					Status:    updateRes.Status,
+					Provider:  updateRes.Provider,
+					CreatedAt: updateRes.CreatedAt.UnixMilli(),
+					UpdatedAt: updateRes.UpdatedAt.UnixMilli(),
+				}))
+			})
+		})
+	})
 })
