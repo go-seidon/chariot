727f674c014468ee0100459c6f0ddd03dc5c499e
KokoRaka <raka.suryaardi@gmail.com>
2022-11-27 23:01:29 +0700

    add get file by id handler implementation

--- internal/resthandler/file.go
+++ internal/resthandler/file.go
@@ -9,6 +9,7 @@ import (
 	"github.com/go-seidon/chariot/internal/storage/multipart"
 	"github.com/go-seidon/provider/serialization"
 	"github.com/go-seidon/provider/status"
+	"github.com/go-seidon/provider/typeconv"
 	"github.com/labstack/echo/v4"
 )
 
@@ -113,6 +114,82 @@ func (h *fileHandler) RetrieveFileBySlug(ctx echo.Context) error {
 		})
 	}
 	return ctx.Stream(http.StatusOK, findFile.Mimetype, findFile.Data)
+}
+
+func (h *fileHandler) GetFileById(ctx echo.Context) error {
+	getFile, err := h.fileClient.GetFileById(ctx.Request().Context(), file.GetFileByIdParam{
+		Id: ctx.Param("id"),
+	})
+	if err != nil {
+		httpCode := http.StatusInternalServerError
+		switch err.Code {
+		case status.INVALID_PARAM:
+			httpCode = http.StatusBadRequest
+		case status.RESOURCE_NOTFOUND:
+			httpCode = http.StatusNotFound
+		}
+		return echo.NewHTTPError(httpCode, &restapp.ResponseBodyInfo{
+			Code:    err.Code,
+			Message: err.Message,
+		})
+	}
+
+	var updatedAt *int64
+	if getFile.UpdatedAt != nil {
+		updatedAt = typeconv.Int64(getFile.UpdatedAt.UnixMilli())
+	}
+
+	var deletedAt *int64
+	if getFile.DeletedAt != nil {
+		deletedAt = typeconv.Int64(getFile.DeletedAt.UnixMilli())
+	}
+
+	locations := []restapp.GetFileByIdLocation{}
+	for _, location := range getFile.Locations {
+		var updatedAt *int64
+		if location.UpdatedAt != nil {
+			updatedAt = typeconv.Int64(location.UpdatedAt.UnixMilli())
+		}
+
+		var uploadedAt *int64
+		if location.UploadedAt != nil {
+			uploadedAt = typeconv.Int64(location.UploadedAt.UnixMilli())
+		}
+
+		locations = append(locations, restapp.GetFileByIdLocation{
+			ExternalId:     location.ExternalId,
+			Priority:       location.Priority,
+			Status:         location.Status,
+			CreatedAt:      location.CreatedAt.UnixMilli(),
+			UpdatedAt:      updatedAt,
+			UploadedAt:     uploadedAt,
+			BarrelId:       location.Barrel.Id,
+			BarrelCode:     location.Barrel.Code,
+			BarrelProvider: location.Barrel.Provider,
+			BarrelStatus:   location.Barrel.Status,
+		})
+	}
+
+	return ctx.JSON(http.StatusOK, &restapp.GetFileByIdResponse{
+		Code:    getFile.Success.Code,
+		Message: getFile.Success.Message,
+		Data: restapp.GetFileByIdData{
+			Id:         getFile.Id,
+			Slug:       getFile.Slug,
+			Name:       getFile.Name,
+			Mimetype:   getFile.Mimetype,
+			Extension:  getFile.Extension,
+			Size:       getFile.Size,
+			Visibility: restapp.GetFileByIdDataVisibility(getFile.Visibility),
+			Status:     restapp.GetFileByIdDataStatus(getFile.Status),
+			UploadedAt: getFile.UploadedAt.Local().UnixMilli(),
+			CreatedAt:  getFile.CreatedAt.UnixMilli(),
+			UpdatedAt:  updatedAt,
+			DeletedAt:  deletedAt,
+			Meta:       &restapp.GetFileByIdData_Meta{AdditionalProperties: getFile.Meta},
+			Locations:  locations,
+		},
+	})
 }
 
 type FileParam struct {
