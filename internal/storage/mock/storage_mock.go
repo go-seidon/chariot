1d1468279d88a36ad40645ec4e0e29481572dc9d
KokoRaka <raka.suryaardi@gmail.com>
2022-11-27 18:39:27 +0700

    add retrieve object hippo implementation

--- internal/storage/mock/storage_mock.go
+++ internal/storage/mock/storage_mock.go
@@ -35,6 +35,21 @@ func (m *MockStorage) EXPECT() *MockStorageMockRecorder {
 	return m.recorder
 }
 
+// RetrieveObject mocks base method.
+func (m *MockStorage) RetrieveObject(ctx context.Context, p storage.RetrieveObjectParam) (*storage.RetrieveObjectResult, error) {
+	m.ctrl.T.Helper()
+	ret := m.ctrl.Call(m, "RetrieveObject", ctx, p)
+	ret0, _ := ret[0].(*storage.RetrieveObjectResult)
+	ret1, _ := ret[1].(error)
+	return ret0, ret1
+}
+
+// RetrieveObject indicates an expected call of RetrieveObject.
+func (mr *MockStorageMockRecorder) RetrieveObject(ctx, p interface{}) *gomock.Call {
+	mr.mock.ctrl.T.Helper()
+	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "RetrieveObject", reflect.TypeOf((*MockStorage)(nil).RetrieveObject), ctx, p)
+}
+
 // UploadObject mocks base method.
 func (m *MockStorage) UploadObject(ctx context.Context, p storage.UploadObjectParam) (*storage.UploadObjectResult, error) {
 	m.ctrl.T.Helper()
