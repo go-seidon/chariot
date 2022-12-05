d1f94b5b1a4ee2b86985dab5fadebe6185795c80
KokoRaka <raka.suryaardi@gmail.com>
2022-12-05 21:21:13 +0700

    add id on file_location

--- migration/mysql/20221124142541_file-location.up.sql
+++ migration/mysql/20221124142541_file-location.up.sql
@@ -1,5 +1,6 @@
 
 CREATE TABLE IF NOT EXISTS `file_location` (
+  `id` VARCHAR(128) NOT NULL,
   `file_id` VARCHAR(128) NOT NULL,
   `barrel_id` VARCHAR(128) NOT NULL,
   `external_id` VARCHAR(256) NULL DEFAULT NULL,
@@ -7,9 +8,10 @@ CREATE TABLE IF NOT EXISTS `file_location` (
   `status` VARCHAR(32) NOT NULL,
   `uploaded_at` BIGINT NULL DEFAULT NULL,
   `created_at` BIGINT NOT NULL,
-  `updated_at` BIGINT NOT NULL
+  `updated_at` BIGINT NOT NULL,
+  `deleted_at` BIGINT NULL DEFAULT NULL,
+  PRIMARY KEY (`id`)
 ) 
 DEFAULT CHARACTER SET utf8mb4
 COLLATE utf8mb4_unicode_ci
 ENGINE = InnoDB;
-
