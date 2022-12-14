9e9b1b9bd70a7e2e28b284bc480048eccc5e6686
KokoRaka <raka.suryaardi@gmail.com>
2022-12-14 22:49:51 +0700

    add delete file message

--- api/queue/message.pb.go
+++ api/queue/message.pb.go
@@ -99,6 +99,69 @@ func (x *ScheduleReplicationMessage) GetStatus() string {
 	return ""
 }
 
+type DeleteFileMessage struct {
+	state         protoimpl.MessageState
+	sizeCache     protoimpl.SizeCache
+	unknownFields protoimpl.UnknownFields
+
+	FileId      string `protobuf:"bytes,1,opt,name=file_id,json=fileId,proto3" json:"file_id,omitempty"`
+	Status      string `protobuf:"bytes,2,opt,name=status,proto3" json:"status,omitempty"`
+	RequestedAt int64  `protobuf:"varint,3,opt,name=requested_at,json=requestedAt,proto3" json:"requested_at,omitempty"`
+}
+
+func (x *DeleteFileMessage) Reset() {
+	*x = DeleteFileMessage{}
+	if protoimpl.UnsafeEnabled {
+		mi := &file_api_queue_message_proto_msgTypes[1]
+		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
+		ms.StoreMessageInfo(mi)
+	}
+}
+
+func (x *DeleteFileMessage) String() string {
+	return protoimpl.X.MessageStringOf(x)
+}
+
+func (*DeleteFileMessage) ProtoMessage() {}
+
+func (x *DeleteFileMessage) ProtoReflect() protoreflect.Message {
+	mi := &file_api_queue_message_proto_msgTypes[1]
+	if protoimpl.UnsafeEnabled && x != nil {
+		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
+		if ms.LoadMessageInfo() == nil {
+			ms.StoreMessageInfo(mi)
+		}
+		return ms
+	}
+	return mi.MessageOf(x)
+}
+
+// Deprecated: Use DeleteFileMessage.ProtoReflect.Descriptor instead.
+func (*DeleteFileMessage) Descriptor() ([]byte, []int) {
+	return file_api_queue_message_proto_rawDescGZIP(), []int{1}
+}
+
+func (x *DeleteFileMessage) GetFileId() string {
+	if x != nil {
+		return x.FileId
+	}
+	return ""
+}
+
+func (x *DeleteFileMessage) GetStatus() string {
+	if x != nil {
+		return x.Status
+	}
+	return ""
+}
+
+func (x *DeleteFileMessage) GetRequestedAt() int64 {
+	if x != nil {
+		return x.RequestedAt
+	}
+	return 0
+}
+
 var File_api_queue_message_proto protoreflect.FileDescriptor
 
 var file_api_queue_message_proto_rawDesc = []byte{
@@ -113,8 +176,15 @@ var file_api_queue_message_proto_rawDesc = []byte{
 	0x72, 0x65, 0x6c, 0x49, 0x64, 0x12, 0x1a, 0x0a, 0x08, 0x70, 0x72, 0x69, 0x6f, 0x72, 0x69, 0x74,
 	0x79, 0x18, 0x04, 0x20, 0x01, 0x28, 0x05, 0x52, 0x08, 0x70, 0x72, 0x69, 0x6f, 0x72, 0x69, 0x74,
 	0x79, 0x12, 0x16, 0x0a, 0x06, 0x73, 0x74, 0x61, 0x74, 0x75, 0x73, 0x18, 0x05, 0x20, 0x01, 0x28,
-	0x09, 0x52, 0x06, 0x73, 0x74, 0x61, 0x74, 0x75, 0x73, 0x42, 0x09, 0x5a, 0x07, 0x2e, 0x2f, 0x71,
-	0x75, 0x65, 0x75, 0x65, 0x62, 0x06, 0x70, 0x72, 0x6f, 0x74, 0x6f, 0x33,
+	0x09, 0x52, 0x06, 0x73, 0x74, 0x61, 0x74, 0x75, 0x73, 0x22, 0x67, 0x0a, 0x11, 0x44, 0x65, 0x6c,
+	0x65, 0x74, 0x65, 0x46, 0x69, 0x6c, 0x65, 0x4d, 0x65, 0x73, 0x73, 0x61, 0x67, 0x65, 0x12, 0x17,
+	0x0a, 0x07, 0x66, 0x69, 0x6c, 0x65, 0x5f, 0x69, 0x64, 0x18, 0x01, 0x20, 0x01, 0x28, 0x09, 0x52,
+	0x06, 0x66, 0x69, 0x6c, 0x65, 0x49, 0x64, 0x12, 0x16, 0x0a, 0x06, 0x73, 0x74, 0x61, 0x74, 0x75,
+	0x73, 0x18, 0x02, 0x20, 0x01, 0x28, 0x09, 0x52, 0x06, 0x73, 0x74, 0x61, 0x74, 0x75, 0x73, 0x12,
+	0x21, 0x0a, 0x0c, 0x72, 0x65, 0x71, 0x75, 0x65, 0x73, 0x74, 0x65, 0x64, 0x5f, 0x61, 0x74, 0x18,
+	0x03, 0x20, 0x01, 0x28, 0x03, 0x52, 0x0b, 0x72, 0x65, 0x71, 0x75, 0x65, 0x73, 0x74, 0x65, 0x64,
+	0x41, 0x74, 0x42, 0x09, 0x5a, 0x07, 0x2e, 0x2f, 0x71, 0x75, 0x65, 0x75, 0x65, 0x62, 0x06, 0x70,
+	0x72, 0x6f, 0x74, 0x6f, 0x33,
 }
 
 var (
@@ -129,9 +199,10 @@ func file_api_queue_message_proto_rawDescGZIP() []byte {
 	return file_api_queue_message_proto_rawDescData
 }
 
-var file_api_queue_message_proto_msgTypes = make([]protoimpl.MessageInfo, 1)
+var file_api_queue_message_proto_msgTypes = make([]protoimpl.MessageInfo, 2)
 var file_api_queue_message_proto_goTypes = []interface{}{
 	(*ScheduleReplicationMessage)(nil), // 0: queue.ScheduleReplicationMessage
+	(*DeleteFileMessage)(nil),          // 1: queue.DeleteFileMessage
 }
 var file_api_queue_message_proto_depIdxs = []int32{
 	0, // [0:0] is the sub-list for method output_type
@@ -159,6 +230,18 @@ func file_api_queue_message_proto_init() {
 				return nil
 			}
 		}
+		file_api_queue_message_proto_msgTypes[1].Exporter = func(v interface{}, i int) interface{} {
+			switch v := v.(*DeleteFileMessage); i {
+			case 0:
+				return &v.state
+			case 1:
+				return &v.sizeCache
+			case 2:
+				return &v.unknownFields
+			default:
+				return nil
+			}
+		}
 	}
 	type x struct{}
 	out := protoimpl.TypeBuilder{
@@ -166,7 +249,7 @@ func file_api_queue_message_proto_init() {
 			GoPackagePath: reflect.TypeOf(x{}).PkgPath(),
 			RawDescriptor: file_api_queue_message_proto_rawDesc,
 			NumEnums:      0,
-			NumMessages:   1,
+			NumMessages:   2,
 			NumExtensions: 0,
 			NumServices:   0,
 		},
