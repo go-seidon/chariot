edfb6a60e91159fcaa63f3ead64adb8f9ba94b44
KokoRaka <raka.suryaardi@gmail.com>
2022-12-16 14:22:26 +0700

    refactor queue implementation

--- internal/queue/queue_test.go
+++ internal/queue/queue_test.go
@@ -3,6 +3,7 @@ package queue_test
 import (
 	"context"
 	"fmt"
+	"sync"
 	"testing"
 
 	"github.com/go-seidon/chariot/internal/queue"
@@ -21,17 +22,18 @@ func TestQueue(t *testing.T) {
 var _ = Describe("Queue Package", func() {
 	Context("Start function", Label("unit"), func() {
 		var (
-			ctx           context.Context
-			que           queue.Queue
-			queuer        *mock_queueing.MockQueuer
-			decExc1Param  queueing.DeclareExchangeParam
-			decQue1Param  queueing.DeclareQueueParam
-			decQue1Res    *queueing.DeclareQueueResult
-			bindQue1Param queueing.BindQueueParam
-			decExc2Param  queueing.DeclareExchangeParam
-			decQue2Param  queueing.DeclareQueueParam
-			decQue2Res    *queueing.DeclareQueueResult
-			bindQue2Param queueing.BindQueueParam
+			ctx    context.Context
+			que    queue.Queue
+			queuer *mock_queueing.MockQueuer
+			wg     *sync.WaitGroup
+			// decExc1Param  queueing.DeclareExchangeParam
+			// decQue1Param  queueing.DeclareQueueParam
+			// decQue1Res    *queueing.DeclareQueueResult
+			// bindQue1Param queueing.BindQueueParam
+			// decExc2Param  queueing.DeclareExchangeParam
+			// decQue2Param  queueing.DeclareQueueParam
+			// decQue2Res    *queueing.DeclareQueueResult
+			// bindQue2Param queueing.BindQueueParam
 		)
 
 		BeforeEach(func() {
@@ -42,34 +44,35 @@ var _ = Describe("Queue Package", func() {
 			que = queue.NewQueue(queue.QueueParam{
 				Queuer: queuer,
 			})
-			decExc1Param = queueing.DeclareExchangeParam{
-				ExchangeName: "file_replication",
-				ExchangeType: "fanout",
-			}
-			decQue1Param = queueing.DeclareQueueParam{
-				QueueName: "proceed_file_replication",
-			}
-			decQue1Res = &queueing.DeclareQueueResult{
-				Name: "proceed_file_replication",
-			}
-			bindQue1Param = queueing.BindQueueParam{
-				ExchangeName: "file_replication",
-				QueueName:    "proceed_file_replication",
-			}
-			decExc2Param = queueing.DeclareExchangeParam{
-				ExchangeName: "file_deletion",
-				ExchangeType: "fanout",
-			}
-			decQue2Param = queueing.DeclareQueueParam{
-				QueueName: "proceed_file_deletion",
-			}
-			decQue2Res = &queueing.DeclareQueueResult{
-				Name: "proceed_file_deletion",
-			}
-			bindQue2Param = queueing.BindQueueParam{
-				ExchangeName: "file_deletion",
-				QueueName:    "proceed_file_deletion",
-			}
+			wg = &sync.WaitGroup{}
+			// decExc1Param = queueing.DeclareExchangeParam{
+			// 	ExchangeName: "file_replication",
+			// 	ExchangeType: "fanout",
+			// }
+			// decQue1Param = queueing.DeclareQueueParam{
+			// 	QueueName: "proceed_file_replication",
+			// }
+			// decQue1Res = &queueing.DeclareQueueResult{
+			// 	Name: "proceed_file_replication",
+			// }
+			// bindQue1Param = queueing.BindQueueParam{
+			// 	ExchangeName: "file_replication",
+			// 	QueueName:    "proceed_file_replication",
+			// }
+			// decExc2Param = queueing.DeclareExchangeParam{
+			// 	ExchangeName: "file_deletion",
+			// 	ExchangeType: "fanout",
+			// }
+			// decQue2Param = queueing.DeclareQueueParam{
+			// 	QueueName: "proceed_file_deletion",
+			// }
+			// decQue2Res = &queueing.DeclareQueueResult{
+			// 	Name: "proceed_file_deletion",
+			// }
+			// bindQue2Param = queueing.BindQueueParam{
+			// 	ExchangeName: "file_deletion",
+			// 	QueueName:    "proceed_file_deletion",
+			// }
 		})
 
 		When("failed open queuer connection", func() {
@@ -86,7 +89,7 @@ var _ = Describe("Queue Package", func() {
 			})
 		})
 
-		When("failed declare exchange file_replication", func() {
+		When("failed declare exchange", func() {
 			It("should return error", func() {
 				queuer.
 					EXPECT().
@@ -94,19 +97,33 @@ var _ = Describe("Queue Package", func() {
 					Return(nil).
 					Times(1)
 
+				wg.Add(4)
+
 				queuer.
 					EXPECT().
-					DeclareExchange(gomock.Eq(ctx), gomock.Eq(decExc1Param)).
-					Return(fmt.Errorf("network error")).
-					Times(1)
+					DeclareExchange(gomock.Eq(ctx), gomock.Any()).
+					DoAndReturn(func(_ context.Context, _ queueing.DeclareExchangeParam) error {
+						wg.Done()
+						return fmt.Errorf("exchange error")
+					}).
+					Times(2)
+
+				queuer.
+					EXPECT().
+					DeclareQueue(gomock.Eq(ctx), gomock.Any()).
+					DoAndReturn(func(_ context.Context, _ queueing.DeclareQueueParam) (*queueing.DeclareQueueResult, error) {
+						wg.Done()
+						return nil, nil
+					}).
+					Times(2)
 
 				err := que.Start(ctx)
 
-				Expect(err).To(Equal(fmt.Errorf("network error")))
+				Expect(err).To(Equal(fmt.Errorf("exchange error")))
 			})
 		})
 
-		When("failed declare queue proceed_file_replication", func() {
+		When("failed declare queue", func() {
 			It("should return error", func() {
 				queuer.
 					EXPECT().
@@ -114,25 +131,33 @@ var _ = Describe("Queue Package", func() {
 					Return(nil).
 					Times(1)
 
-				queuer.
-					EXPECT().
-					DeclareExchange(gomock.Eq(ctx), gomock.Eq(decExc1Param)).
-					Return(nil).
-					Times(1)
+				wg.Add(4)
 
 				queuer.
 					EXPECT().
-					DeclareQueue(gomock.Eq(ctx), gomock.Eq(decQue1Param)).
-					Return(nil, fmt.Errorf("network error")).
-					Times(1)
+					DeclareExchange(gomock.Eq(ctx), gomock.Any()).
+					DoAndReturn(func(_ context.Context, _ queueing.DeclareExchangeParam) error {
+						wg.Done()
+						return nil
+					}).
+					Times(2)
+
+				queuer.
+					EXPECT().
+					DeclareQueue(gomock.Eq(ctx), gomock.Any()).
+					DoAndReturn(func(_ context.Context, _ queueing.DeclareQueueParam) (*queueing.DeclareQueueResult, error) {
+						wg.Done()
+						return nil, fmt.Errorf("queue error")
+					}).
+					Times(2)
 
 				err := que.Start(ctx)
 
-				Expect(err).To(Equal(fmt.Errorf("network error")))
+				Expect(err).To(Equal(fmt.Errorf("queue error")))
 			})
 		})
 
-		When("failed bind queue proceed_file_replication", func() {
+		When("failed bind queue", func() {
 			It("should return error", func() {
 				queuer.
 					EXPECT().
@@ -140,31 +165,42 @@ var _ = Describe("Queue Package", func() {
 					Return(nil).
 					Times(1)
 
-				queuer.
-					EXPECT().
-					DeclareExchange(gomock.Eq(ctx), gomock.Eq(decExc1Param)).
-					Return(nil).
-					Times(1)
+				wg.Add(6)
 
 				queuer.
 					EXPECT().
-					DeclareQueue(gomock.Eq(ctx), gomock.Eq(decQue1Param)).
-					Return(decQue1Res, nil).
-					Times(1)
+					DeclareExchange(gomock.Eq(ctx), gomock.Any()).
+					DoAndReturn(func(_ context.Context, _ queueing.DeclareExchangeParam) error {
+						wg.Done()
+						return nil
+					}).
+					Times(2)
 
 				queuer.
 					EXPECT().
-					BindQueue(gomock.Eq(ctx), gomock.Eq(bindQue1Param)).
-					Return(fmt.Errorf("network error")).
-					Times(1)
+					DeclareQueue(gomock.Eq(ctx), gomock.Any()).
+					DoAndReturn(func(_ context.Context, _ queueing.DeclareQueueParam) (*queueing.DeclareQueueResult, error) {
+						wg.Done()
+						return nil, nil
+					}).
+					Times(2)
+
+				queuer.
+					EXPECT().
+					BindQueue(gomock.Eq(ctx), gomock.Any()).
+					DoAndReturn(func(_ context.Context, _ queueing.BindQueueParam) error {
+						wg.Done()
+						return fmt.Errorf("queue error")
+					}).
+					Times(2)
 
 				err := que.Start(ctx)
 
-				Expect(err).To(Equal(fmt.Errorf("network error")))
+				Expect(err).To(Equal(fmt.Errorf("queue error")))
 			})
 		})
 
-		When("failed declare exchange file_deletion", func() {
+		When("failed subscribe", func() {
 			It("should return error", func() {
 				queuer.
 					EXPECT().
@@ -172,183 +208,45 @@ var _ = Describe("Queue Package", func() {
 					Return(nil).
 					Times(1)
 
-				queuer.
-					EXPECT().
-					DeclareExchange(gomock.Eq(ctx), gomock.Eq(decExc1Param)).
-					Return(nil).
-					Times(1)
+				wg.Add(8)
 
 				queuer.
 					EXPECT().
-					DeclareQueue(gomock.Eq(ctx), gomock.Eq(decQue1Param)).
-					Return(decQue1Res, nil).
-					Times(1)
+					DeclareExchange(gomock.Eq(ctx), gomock.Any()).
+					DoAndReturn(func(_ context.Context, _ queueing.DeclareExchangeParam) error {
+						return nil
+					}).
+					Times(2)
 
 				queuer.
 					EXPECT().
-					BindQueue(gomock.Eq(ctx), gomock.Eq(bindQue1Param)).
-					Return(nil).
-					Times(1)
+					DeclareQueue(gomock.Eq(ctx), gomock.Any()).
+					DoAndReturn(func(_ context.Context, _ queueing.DeclareQueueParam) (*queueing.DeclareQueueResult, error) {
+						return nil, nil
+					}).
+					Times(2)
 
 				queuer.
 					EXPECT().
-					DeclareExchange(gomock.Eq(ctx), gomock.Eq(decExc2Param)).
-					Return(fmt.Errorf("network error")).
-					Times(1)
-
-				err := que.Start(ctx)
-
-				Expect(err).To(Equal(fmt.Errorf("network error")))
-			})
-		})
-
-		When("failed declare queue proceed_file_deletion", func() {
-			It("should return error", func() {
-				queuer.
-					EXPECT().
-					Open(gomock.Eq(ctx)).
-					Return(nil).
-					Times(1)
-
-				queuer.
-					EXPECT().
-					DeclareExchange(gomock.Eq(ctx), gomock.Eq(decExc1Param)).
-					Return(nil).
-					Times(1)
-
-				queuer.
-					EXPECT().
-					DeclareQueue(gomock.Eq(ctx), gomock.Eq(decQue1Param)).
-					Return(decQue1Res, nil).
-					Times(1)
-
-				queuer.
-					EXPECT().
-					BindQueue(gomock.Eq(ctx), gomock.Eq(bindQue1Param)).
-					Return(nil).
-					Times(1)
-
-				queuer.
-					EXPECT().
-					DeclareExchange(gomock.Eq(ctx), gomock.Eq(decExc2Param)).
-					Return(nil).
-					Times(1)
-
-				queuer.
-					EXPECT().
-					DeclareQueue(gomock.Eq(ctx), gomock.Eq(decQue2Param)).
-					Return(nil, fmt.Errorf("network error")).
-					Times(1)
-
-				err := que.Start(ctx)
-
-				Expect(err).To(Equal(fmt.Errorf("network error")))
-			})
-		})
-
-		When("failed bind queue proceed_file_deletion", func() {
-			It("should return error", func() {
-				queuer.
-					EXPECT().
-					Open(gomock.Eq(ctx)).
-					Return(nil).
-					Times(1)
-
-				queuer.
-					EXPECT().
-					DeclareExchange(gomock.Eq(ctx), gomock.Eq(decExc1Param)).
-					Return(nil).
-					Times(1)
-
-				queuer.
-					EXPECT().
-					DeclareQueue(gomock.Eq(ctx), gomock.Eq(decQue1Param)).
-					Return(decQue1Res, nil).
-					Times(1)
-
-				queuer.
-					EXPECT().
-					BindQueue(gomock.Eq(ctx), gomock.Eq(bindQue1Param)).
-					Return(nil).
-					Times(1)
-
-				queuer.
-					EXPECT().
-					DeclareExchange(gomock.Eq(ctx), gomock.Eq(decExc2Param)).
-					Return(nil).
-					Times(1)
-
-				queuer.
-					EXPECT().
-					DeclareQueue(gomock.Eq(ctx), gomock.Eq(decQue2Param)).
-					Return(decQue2Res, nil).
-					Times(1)
-
-				queuer.
-					EXPECT().
-					BindQueue(gomock.Eq(ctx), gomock.Eq(bindQue2Param)).
-					Return(fmt.Errorf("network error")).
-					Times(1)
-
-				err := que.Start(ctx)
-
-				Expect(err).To(Equal(fmt.Errorf("network error")))
-			})
-		})
-
-		When("failed subscribe proceed_file_replication", func() {
-			It("should return error", func() {
-				queuer.
-					EXPECT().
-					Open(gomock.Eq(ctx)).
-					Return(nil).
-					Times(1)
-
-				queuer.
-					EXPECT().
-					DeclareExchange(gomock.Eq(ctx), gomock.Eq(decExc1Param)).
-					Return(nil).
-					Times(1)
-
-				queuer.
-					EXPECT().
-					DeclareQueue(gomock.Eq(ctx), gomock.Eq(decQue1Param)).
-					Return(decQue1Res, nil).
-					Times(1)
-
-				queuer.
-					EXPECT().
-					BindQueue(gomock.Eq(ctx), gomock.Eq(bindQue1Param)).
-					Return(nil).
-					Times(1)
-
-				queuer.
-					EXPECT().
-					DeclareExchange(gomock.Eq(ctx), gomock.Eq(decExc2Param)).
-					Return(nil).
-					Times(1)
-
-				queuer.
-					EXPECT().
-					DeclareQueue(gomock.Eq(ctx), gomock.Eq(decQue2Param)).
-					Return(decQue2Res, nil).
-					Times(1)
-
-				queuer.
-					EXPECT().
-					BindQueue(gomock.Eq(ctx), gomock.Eq(bindQue2Param)).
-					Return(nil).
-					Times(1)
+					BindQueue(gomock.Eq(ctx), gomock.Any()).
+					DoAndReturn(func(_ context.Context, _ queueing.BindQueueParam) error {
+						wg.Done()
+						return nil
+					}).
+					Times(2)
 
 				queuer.
 					EXPECT().
 					Subscribe(gomock.Eq(ctx), gomock.Any()).
-					Return(fmt.Errorf("network error")).
-					Times(1)
+					DoAndReturn(func(_ context.Context, _ queueing.SubscribeParam) error {
+						wg.Done()
+						return fmt.Errorf("subscribe error")
+					}).
+					Times(2)
 
 				err := que.Start(ctx)
 
-				Expect(err).To(Equal(fmt.Errorf("network error")))
+				Expect(err).To(Equal(fmt.Errorf("subscribe error")))
 			})
 		})
 
@@ -360,47 +258,41 @@ var _ = Describe("Queue Package", func() {
 					Return(nil).
 					Times(1)
 
-				queuer.
-					EXPECT().
-					DeclareExchange(gomock.Eq(ctx), gomock.Eq(decExc1Param)).
-					Return(nil).
-					Times(1)
+				wg.Add(8)
 
 				queuer.
 					EXPECT().
-					DeclareQueue(gomock.Eq(ctx), gomock.Eq(decQue1Param)).
-					Return(decQue1Res, nil).
-					Times(1)
+					DeclareExchange(gomock.Eq(ctx), gomock.Any()).
+					DoAndReturn(func(_ context.Context, _ queueing.DeclareExchangeParam) error {
+						return nil
+					}).
+					Times(2)
 
 				queuer.
 					EXPECT().
-					BindQueue(gomock.Eq(ctx), gomock.Eq(bindQue1Param)).
-					Return(nil).
-					Times(1)
+					DeclareQueue(gomock.Eq(ctx), gomock.Any()).
+					DoAndReturn(func(_ context.Context, _ queueing.DeclareQueueParam) (*queueing.DeclareQueueResult, error) {
+						return nil, nil
+					}).
+					Times(2)
 
 				queuer.
 					EXPECT().
-					DeclareExchange(gomock.Eq(ctx), gomock.Eq(decExc2Param)).
-					Return(nil).
-					Times(1)
-
-				queuer.
-					EXPECT().
-					DeclareQueue(gomock.Eq(ctx), gomock.Eq(decQue2Param)).
-					Return(decQue2Res, nil).
-					Times(1)
-
-				queuer.
-					EXPECT().
-					BindQueue(gomock.Eq(ctx), gomock.Eq(bindQue2Param)).
-					Return(nil).
-					Times(1)
+					BindQueue(gomock.Eq(ctx), gomock.Any()).
+					DoAndReturn(func(_ context.Context, _ queueing.BindQueueParam) error {
+						wg.Done()
+						return nil
+					}).
+					Times(2)
 
 				queuer.
 					EXPECT().
 					Subscribe(gomock.Eq(ctx), gomock.Any()).
-					Return(nil).
-					Times(1)
+					DoAndReturn(func(_ context.Context, _ queueing.SubscribeParam) error {
+						wg.Done()
+						return nil
+					}).
+					Times(2)
 
 				err := que.Start(ctx)
 
